eaad725a45f7366d4c10d315ebfc24014930e0cf
Jon Carvajal <jon.c.carvajal@gmail.com>
2019-03-25 19:13:01 -0400

    Added CTRL Click functionality

--- assets/js/country.js
+++ assets/js/country.js
@@ -1,3 +1,29 @@
+var multiMode = false;
+var selectedNodes = {};
+
+$(document).keydown(function (event) {
+    if (!multiMode && event.which == "17"){
+        multiMode = true;
+    }
+});
+
+$(document).keyup(function () {
+    multiMode = false;
+    console.log(selectedNodes);//Do things
+    if(Object.keys(selectedNodes).length == 1){
+        console.log("Single Element Selected");
+        var node = selectedNodes[Object.keys(selectedNodes)[0]];
+        singleSelectState(node["html"], node["state"]);
+        selectedNodes = {};
+    } else if(Object.keys(selectedNodes).length > 1) {
+        console.log("Multi Select deactivated, comparing elements");
+        //CALL COMPARISON FUNCTION HERE
+        multiSelectState();
+
+    }
+    //Else do nothing
+});
+
 var api_key = "yAJchpcn7oDJXIWQQQxNvRAnqwne6Stw3ounmUCW";
 
 var mapSvg = d3.select("#map").select("svg");
@@ -49,90 +75,90 @@ $('#selection-modal').click(function (e) {
 });
 
 function clicked(d) {
-    if (active.node() === this) return reset();
-    
-    var stateName = stateMap[d.id]["name"];
-	var stateAbbr = stateMap[d.id]["abbr"];
-    $('#state-header').text(stateName);
-	var chartSvg = d3.select("#state-content").select("svg");
-	
-	//-------- JLM add a "chart" to modal --------------
-	//TODO - why is the svg element stuck at the size it is? 
-	//			in inspect element, it has it as 300x150, regardless of what I set it to below. any ideas?
-	chartSvg.append("svg")
-		.attr("width", 300)
-		.attr("height", 300);
-	
-	var jsonCircles = [
-		{ "x_axis": 50, "y_axis": 50, "radius": 20, "color" : "green" },
-		{ "x_axis": 75, "y_axis": 75, "radius": 20, "color" : "purple"},
-		{ "x_axis": 100, "y_axis": 100, "radius": 20, "color" : "red"},
-		{ "x_axis": 125, "y_axis": 125, "radius": 20, "color" : "black"},
-		{ "x_axis": 150, "y_axis": 150, "radius": 20, "color" : "blue"},
-		{ "x_axis": 150, "y_axis": 50, "radius": 20, "color" : "green" },
-		{ "x_axis": 175, "y_axis": 75, "radius": 20, "color" : "purple"},
-		{ "x_axis": 200, "y_axis": 100, "radius": 20, "color" : "red"},
-		{ "x_axis": 225, "y_axis": 125, "radius": 20, "color" : "black"},
-		{ "x_axis": 250, "y_axis": 150, "radius": 20, "color" : "blue"},
-		{ "x_axis": 250, "y_axis": 50, "radius": 20, "color" : "green" },
-		{ "x_axis": 275, "y_axis": 75, "radius": 20, "color" : "purple"},
-		{ "x_axis": 300, "y_axis": 100, "radius": 20, "color" : "red"},
-		{ "x_axis": 325, "y_axis": 125, "radius": 20, "color" : "black"},
-		{ "x_axis": 350, "y_axis": 150, "radius": 20, "color" : "blue"}];
+    if (multiMode) {
+        if(selectedNodes[d.id]){
+            delete selectedNodes[d.id];
+            d3.select(this).classed("active", false);
+        }else{
+            selectedNodes[d.id] = {"html":this, "state":d};
+            d3.select(this).classed("active", true);
+        }
+    } else {
+        singleSelectState(this, d);
+    }
+}
 
-	var circles = chartSvg.selectAll("circle")
-                          .data(jsonCircles)
-                          .enter()
-                          .append("circle");
+function showModal(){
+    $('#selection-modal-dialog').animate({ right: '0%' });
+    $('#selection-modal').css('pointer-events', 'all');
+}
 
-	var circleAttributes = circles
-                       .attr("cx", function (d) { return d.x_axis; })
-                       .attr("cy", function (d) { return d.y_axis; })
-                       .attr("r", function (d) { return d.radius; })
-                       .style("fill", function(d) { return d.color; });
-	//-------- End JLM add a "chart" to modal --------------
-
-    var estimateUrl = "http://127.0.0.1:7777/https://api.usa.gov/crime/fbi/sapi/api/estimates/states/" + stateMap[d.id]["abbr"] + "/1990/2018?api_key=" + api_key;
-
-    $.ajax({url: estimateUrl,
-        type: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            "cache-control": "no-cache",
-        },
-        success: function(result){
-            console.log(result)
-      }});
-
-
-    active.classed("active", false);
-    active = d3.select(this).classed("active", true);
-
-    var bounds = path.bounds(d),
-        dx = bounds[1][0] - bounds[0][0],
-        dy = bounds[1][1] - bounds[0][1],
-        x = (bounds[0][0] + bounds[1][0]) / 2,
-        y = (bounds[0][1] + bounds[1][1]) / 2,
-        scale = .5 / Math.max(dx / width, dy / height),
-        translate = [(width / 2 - scale * x) - width / 4, height / 2 - scale * y];
-
-    g.transition()
-        .duration(750)
-        .style("stroke-width", 1.5 / scale + "px")
-        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-
-   $('#selection-modal-dialog').animate({right:'0%'});
-   $('#selection-modal').css('pointer-events', 'all');
+function hideModal(){
+    $('#selection-modal-dialog').animate({ right: '-50%' });
+    $('#selection-modal').css('pointer-events', 'none');
 }
 
 function reset() {
-    $('#selection-modal-dialog').animate({right: '-50%'});
-    $('#selection-modal').css('pointer-events', 'none');
+    hideModal();
+    $('#state-header').text("");
     active.classed("active", false);
     active = d3.select(null);
+
+    for(var key in selectedNodes){
+        d3.select(selectedNodes[key]["html"]).classed("active", false);
+    }
+    selectedNodes = {};
 
     g.transition()
         .duration(750)
         .style("stroke-width", "1.5px")
         .attr("transform", "");
+}
+
+function singleSelectState(html, d){
+    if (active.node() === html) return reset();
+
+        var stateName = stateMap[d.id]["name"];
+        var stateAbbr = stateMap[d.id]["abbr"];
+        $('#state-header').text(stateName);
+        var estimateUrl = "http://127.0.0.1:7777/https://api.usa.gov/crime/fbi/sapi/api/estimates/states/" + stateMap[d.id]["abbr"] + "/1990/2018?api_key=" + api_key;
+
+        $.ajax({
+            url: estimateUrl,
+            type: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "cache-control": "no-cache",
+            },
+            success: function (result) {
+                console.log(result)
+                
+
+            }
+        });
+
+
+        active.classed("active", false);
+        active = d3.select(html).classed("active", true);
+
+        var bounds = path.bounds(d),
+            dx = bounds[1][0] - bounds[0][0],
+            dy = bounds[1][1] - bounds[0][1],
+            x = (bounds[0][0] + bounds[1][0]) / 2,
+            y = (bounds[0][1] + bounds[1][1]) / 2,
+            scale = .5 / Math.max(dx / width, dy / height),
+            translate = [(width / 2 - scale * x) - width / 4, height / 2 - scale * y];
+
+        g.transition()
+            .duration(750)
+            .style("stroke-width", 1.5 / scale + "px")
+            .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+
+            showModal();
+}
+
+function multiSelectState(){
+    //Populate modal with svg
+    $('#state-header').text("State Comparison");
+    showModal();
 }
